76060cd2128c4404ff77ae7e8a9808ebde25f7ed
pgm-jandeschacht <jandesch@student.arteveldehs.be>
2021-11-19 21:35:53 +0100

    Tweak in starting position

--- src/App/index.jsx
+++ src/App/index.jsx
@@ -3,7 +3,7 @@ import Content from "./Content";
 
 function App() {
   return (
-      <Canvas shadows shadowMap orthographic camera={{ zoom: 70, position: [25, 30, 25] }}>
+      <Canvas shadows shadowMap orthographic camera={{ zoom: 55, position: [12, 9, 7] }}>
           <Content linear={false} shadows={true} />
       </Canvas>
   );
